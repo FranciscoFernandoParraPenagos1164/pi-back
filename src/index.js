8271053a66e026b9c96eeb2bb671def77a96390a
Francisco Parra <francisco.parra.penagos@gmail.com>
2023-04-30 21:29:04 -0500

    create nurses crud

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import indexRoutes from './routes/index.routes.js'
 import clientsRoutes from './routes/clients.routes.js'
 import patientsRoutes from './routes/patients.routes.js'
 import medicalConditions from './routes/medicalCondition.routes.js'
+import nursesRoutes from './routes/nurse.routes.js'
 import { badRequest } from './middlewares/bad-request.middlewares.js'
 dotenv.config()
 
@@ -15,6 +16,7 @@ app.use(indexRoutes)
 app.use('/clients', clientsRoutes)
 app.use('/patients', patientsRoutes)
 app.use('/medical-conditions', medicalConditions)
+app.use('/nurses', nursesRoutes)
 app.use(badRequest)
 
 app.listen(PORT, () => {
