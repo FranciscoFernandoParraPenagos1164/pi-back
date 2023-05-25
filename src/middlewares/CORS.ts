8e5b8cfcef7309f7652af636ac5d2307f8cb657c
Francisco Fernando Parra Penagos <francisco.parra.penagos@gmail.com>
2023-05-24 21:11:44 -0500

    modify CORS policy

--- src/middlewares/CORS.ts
+++ src/middlewares/CORS.ts
@@ -1,8 +1,17 @@
 import { Request, Response, NextFunction } from 'express'
 
 export function CORS(_req: Request, res: Response, next: NextFunction) {
-  res.append('Access-Control-Allow-Origin', ['*'])
-  res.append('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
-  res.append('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, OPTIONS'
+  )
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  res.header('Allow', 'GET, POST, PATC, DELETE, OPTIONS')
+
+  if (_req.method === 'OPTIONS') {
+    return res.status(200).end()
+  }
+
   next()
 }
